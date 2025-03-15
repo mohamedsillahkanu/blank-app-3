2af3e672d3d145bca0b68fa11c5bb8c46e94d2f3
mohamedsillahkanu <114839447+mohamedsillahkanu@users.noreply.github.com>
2025-03-15 13:07:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ particles_js = """
         #particles-js {
             position: fixed;
             width: 100%;
-            height: 100%;
+            height: 20%;
             top: 0;
             left: 0;
             right: 0;
